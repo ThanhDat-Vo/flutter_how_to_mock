b27452f81d10c14ad4b9bfda8b9e346445d748ab
thanh-vo-opun-ai <thanh.vo@opun.ai>
2021-03-26 00:32:40 -0700

    update

--- test/services/time_service_test.dart
+++ test/services/time_service_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:how_to_mock/services/time_service.dart';
 
 main() {
-  test("Expect morning", () async {
+  test("Expect morning", () {
     final timeService = TimeService();
     String time =
-        await timeService.getCurrentTime(DateTime.parse('2021-03-25 08:39:26'));
+        timeService.getCurrentTime(DateTime.parse('2021-03-25 08:39:26'));
     expect(time, "Morning");
   });
 }
